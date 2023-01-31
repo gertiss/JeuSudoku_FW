d131046881b5da624b9d6d7755c7acb5a378c69d
Gerard Tisseau <gerard.tisseau@free.fr>
2023-01-31 17:46:41 +0100

    Initial Commit

---
+++ JeuSudoku_FW/JeuSudoku_FW.h
@@ -0,0 +1,18 @@
+//
+//  JeuSudoku_FW.h
+//  JeuSudoku_FW
+//
+//  Created by Gérard Tisseau on 31/01/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JeuSudoku_FW.
+FOUNDATION_EXPORT double JeuSudoku_FWVersionNumber;
+
+//! Project version string for JeuSudoku_FW.
+FOUNDATION_EXPORT const unsigned char JeuSudoku_FWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JeuSudoku_FW/PublicHeader.h>
+
+
